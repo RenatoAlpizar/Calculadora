24d68ec15dceee70e6d1afa3233eed6c6c14e2ed
Renato Alpízar López <87439207+RenatoAlpizar@users.noreply.github.com>
2021-10-11 23:00:15 -0600

    Add files via upload
    Script final, permite varias operaciones

--- script.js
+++ script.js
@@ -9,51 +9,52 @@ var textoEnPantalla;
 var acum;
 var tipoSigno;
 var resultado=0;
-
 function agregar_numero(text){
     textoEnPantalla = document.getElementById("resultado").value;
     pantalla=text;
     var punto=".";
     var fin=textoEnPantalla.charAt(textoEnPantalla.length-1);
-    
     if(textoEnPantalla.length===0){
-        
-        contenido=document.getElementById("resultado").value=text;   
+        contenido=document.getElementById("resultado").value=text;  
+    
         if(fin==="."){ 
         fin=".";
         }
-        
         if(text===fin){
           document.getElementById("resultado").value=textoEnPantalla ;
         }
-        
-    }else{        
+    }else{
+          
            contenido=document.getElementById("resultado").value=textoEnPantalla+text;
             if(fin==="."){ 
             fin=".";
             }
-            
             if(text===fin){
             contenido=document.getElementById("resultado").value=textoEnPantalla ;
-            }         
-        }  
+            }
+            
+        }
+    
+       
+    
 }
+
 
 function tipoOperacion(signo){
     document.getElementById("resultado").value=contenido+signo;
     tipoSigno=signo;
-  
+    
+
+    
 }
 function procesar(){      
         var total=0;
         if(tipoOperacion(tipoSigno)!==0){
         total=eval(contenido);
-        document.getElementById("resultado").value=total;
+        contenido=document.getElementById("resultado").value=total;
         
-      }  
+      }
+      tipoSigno=0;
 }
-
-
-
 
 
